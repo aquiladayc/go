749c66c5e238bc6538ee1de373c4717200930208
aquiladayc <akira.hiiro@datadoghq.com>
2020-02-16 04:18:57 +0000

    rewrote a test code

--- helloworld/deck_test.go
+++ helloworld/deck_test.go
@@ -1,7 +1,9 @@
 package main
 
-import "testing"
-
+import (
+	"testing"
+        "os"
+)
 func TestNewDeck(t *testing.T) {
     d := newDeck()
     if len(d) != 16 {
@@ -11,4 +13,19 @@ func TestNewDeck(t *testing.T) {
     if d[0] != "Ace of Spades" {
         t.Errorf("First cards should be Ace of Spades but got %v", d[0])
     }
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+    os.Remove("_decktesting")
+
+    d := newDeck()
+    d.saveToFile("_decktesting")
+    
+    loadedDeck := newDeckFromFile("_decktesting")
+
+    if len(loadedDeck) != 16 {
+        t.Errorf("Expected deck length of 16, but got %v", len(d))
+    }
+
+    os.Remove("_decktesting")
 }
